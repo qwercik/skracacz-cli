9e543f503452e0bd767e61117353e4d9367cb307
qwercik <erykandrzejewski@gmail.com>
2019-10-27 23:53:16 +0100

    Validate input data

--- skracacz.py
+++ skracacz.py
@@ -1,30 +1,54 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
 FRONTEND_URL = 'https://s.komputeryk.pl'
 API_URL = 'https://s.komputeryk.pl/api'
+
+EXIT_SUCCESS = 0
+EXIT_INCORRECT_USAGE = 1
+EXIT_INVALID_URL = 2
+EXIT_INVALID_TOKEN = 3
+
 
 def printHelp(file=sys.stdout):
     print('Use:', sys.argv[0], '<url>', '[token]', file=file)
     print('<url> - link you want to shorten', file=file)
     print('[token] - (optional) your shortcut name (e.g. /helloworld)', file=file)
 
+def urlValid(url):
+    return re.match(r'^(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})$', url)
+
+def tokenValid(token):
+    return re.match(r'^[A-Za-z0-9]+$', token)
+
 def main():
     if len(sys.argv) < 2 or len(sys.argv) > 3:
         print('Incorrect usage', file=sys.stderr)
         printHelp(sys.stderr)
+        sys.exit(EXIT_INCORRECT_USAGE)
     elif sys.argv[1] in ['-h', '--help']:
         printHelp(sys.stdout)
-    else:
-        url = sys.argv[1]
-        data = { 'url': url }
+        sys.exit(EXIT_SUCCESS)
+        
+    url = sys.argv[1]
+    data = { 'url': url }
+    
+    if not urlValid(url):
+        print('Invalid URL', file=sys.stderr)
+        sys.exit(EXIT_INVALID_URL)
+    
+    if len(sys.argv) == 3:
+        token = sys.argv[2]
 
-        if len(sys.argv) == 3:
-            token = sys.argv[2]
-            data = { **data, 'token': token }
+        if not tokenValid(token):
+            print('Invalid token', file=sys.stderr)
+            sys.exit(EXIT_INVALID_TOKEN)
 
-        print(data)
+        data = { **data, 'token': token }
+
+    print(data)
 
 
 if __name__ == '__main__':
