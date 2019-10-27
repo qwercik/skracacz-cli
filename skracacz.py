0c32aed8da9eec9c436c7c8be6faa2bfc840049c
qwercik <erykandrzejewski@gmail.com>
2019-10-27 23:45:44 +0100

    Handle CLI

--- skracacz.py
+++ skracacz.py
@@ -1,7 +1,31 @@
 #!/usr/bin/env python3
 
+import sys
+
+FRONTEND_URL = 'https://s.komputeryk.pl'
+API_URL = 'https://s.komputeryk.pl/api'
+
+def printHelp(file=sys.stdout):
+    print('Use:', sys.argv[0], '<url>', '[token]', file=file)
+    print('<url> - link you want to shorten', file=file)
+    print('[token] - (optional) your shortcut name (e.g. /helloworld)', file=file)
+
 def main():
-    print('Hello, world')
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        print('Incorrect usage', file=sys.stderr)
+        printHelp(sys.stderr)
+    elif sys.argv[1] in ['-h', '--help']:
+        printHelp(sys.stdout)
+    else:
+        url = sys.argv[1]
+        data = { 'url': url }
+
+        if len(sys.argv) == 3:
+            token = sys.argv[2]
+            data = { **data, 'token': token }
+
+        print(data)
+
 
 if __name__ == '__main__':
     main()
